15c8ad68af00e591cb8485a44834c540cbe8099d
eric.risco <erisco@icloud.com>
2023-01-28 01:56:45 +0100

    Project config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server'
+	integrations: [tailwind(), react()],
+	output: 'server'
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                twitch: [
+                    'Inter',
+                    'Roobert',
+                    'Helvetica Neue',
+                    'Helvetica',
+                    'Arial',
+                    'sans-serif'
+                ]
+            }
+		}
 	},
-	plugins: [],
-}
+	plugins: []
+};
