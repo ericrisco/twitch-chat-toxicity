39172ffa752870c449894eb81d97c0bad7171770
eric.risco <erisco@icloud.com>
2023-01-31 01:54:50 +0100

    Twitch isOnline server function

--- api/index.js
+++ api/index.js
@@ -28,13 +28,37 @@ const port = config.get('server.port');
 cohere.init(process.env.COHERE_API_KEY);
 
 // Routing
-app.post('/api/twitch/message', async (req, res) => {
+app.post('/api/twitch/classify', async (req, res) => {
 	const response = await cohere.classify({
 		model: 'large',
 		inputs: [req.body.message],
 		examples: messages
 	});
 	res.json({ response: response.body.classifications });
+});
+
+app.get('/api/twitch/userinfo', async (req, res) => {
+	try {
+		const clientId = process.env.TWITCH_CLIENT_ID;
+		const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
+		const authUrl = config.get('TWITCH_AUTH_URL').replace('##CLIENT_ID##', clientId).replace('##CLIENT_SECRET##', clientSecret);
+		const responseToken = await fetch(authUrl, { method: 'POST' });
+		const dataToken = await responseToken.json();
+		const token = dataToken.access_token;
+
+		const responseInfo = await fetch(`${config.get('TWITCH_QUERY_URL')}${req.query.userName}`, {
+			headers: { Authorization: `Bearer ${token}`, 'Client-ID': clientId }
+		});
+		const dataInfo = await responseInfo.json();
+
+		const userInfo = dataInfo.data.filter((user) => user.broadcaster_login.toLowerCase() === req.query.userName.toLowerCase())[0];
+
+		res.json({ isOnline: userInfo.is_live, avatarUrl: userInfo.thumbnail_url });
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ isOnline: false, avatarUrl: '', error: err.toString() });
+	}
 });
 
 app.listen(port, async () =>
