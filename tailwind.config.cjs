e844ea56a7e90dab8841329c504d25e8ddf9d397
eric.risco <erisco@icloud.com>
2023-01-31 01:53:49 +0100

    Tailwind reconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,16 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-            fontFamily: {
-                twitch: [
-                    'Inter',
-                    'Roobert',
-                    'Helvetica Neue',
-                    'Helvetica',
-                    'Arial',
-                    'sans-serif'
-                ]
-            }
+			fontFamily: {
+				twitch: [
+					'Inter',
+					'Roobert',
+					'Helvetica Neue',
+					'Helvetica',
+					'Arial',
+					'sans-serif'
+				]
+			}
 		}
 	},
 	plugins: []
