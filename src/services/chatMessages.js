78d6f9e4d127e48da0bc784ddeea2af4517ab26d
eric.risco <erisco@icloud.com>
2023-01-31 01:55:04 +0100

    React Services

--- src/services/chatMessages.js
+++ src/services/chatMessages.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Tmi from 'react-tmi';
 
 const WINDOW_SIZE = 30;
@@ -8,10 +8,18 @@ const startMessage = {
 	userName: 'TwitchToxicTracker'
 };
 
-const randomColors = ['#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
+const randomColors = [
+	'#3498db',
+	'#2ecc71',
+	'#9b59b6',
+	'#f1c40f',
+	'#e67e22',
+	'#e74c3c'
+];
 
 export default function chatMessages({ userName }) {
 	let [messages, setMessages] = useState([startMessage]);
+	const [chatConnected, setChatConnected] = useState(false);
 
 	const client = useRef(
 		new Tmi.Client({
@@ -23,17 +31,27 @@ export default function chatMessages({ userName }) {
 		client.current.connect();
 		client.current.on('message', (channel, tags, message, self) => {
 			if (self) return;
-			const randomColor = randomColors[Math.floor(Math.random() * randomColors.length)];
-			const newMessage = { message, userName: tags['display-name'], color: randomColor };
-			if (messages.filter(e => e.message === message && e.userName === newMessage.userName).length === 0) {
-				messages = [
-					...messages.slice(-WINDOW_SIZE),
-					newMessage
-				];
+			const randomColor =
+				randomColors[Math.floor(Math.random() * randomColors.length)];
+			const newMessage = {
+				message,
+				userName: tags['display-name'],
+				color: randomColor
+			};
+			if (
+				messages.filter(
+					(e) => e.message === message && e.userName === newMessage.userName
+				).length === 0
+			) {
+				messages = [...messages.slice(-WINDOW_SIZE), newMessage];
 				setMessages(messages);
+				setChatConnected(true);
 			}
 		});
 	}, [messages]);
 
-	return messages;
+	return {
+		messages,
+		chatConnected
+	};
 }
