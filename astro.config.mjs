b272a5ea5238a9f92eb9fae2ccdc9df63eb7bb6b
eric.risco <erisco@icloud.com>
2023-02-03 01:55:11 +0100

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: 'server'
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel()
 });
