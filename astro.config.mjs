070904de705372a8aeec267c6232ddf998df05bc
eric.risco <erisco@icloud.com>
2023-02-03 16:37:03 +0100

    Vercel + Node

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,8 @@ import react from '@astrojs/react';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
